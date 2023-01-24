7689de0491ea31d9859854450c879e913390e781
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-24 11:27:25 -0800

    add function component & placeholder

---
+++ src/GuessControlFuncComponent.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GuessControlFuncComponent() {
+  return (
+    <div>GuessControlFuncComponent Placeholder</div>
+  )
+}
+
+export default GuessControlFuncComponent
