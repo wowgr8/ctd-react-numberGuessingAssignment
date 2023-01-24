4340493e5c7d0fed1ceef8e8074c7e7490821a74
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-24 11:57:26 -0800

    add multi-line return from NumberGuessingGame, remove all references to 'this.state' - WIP

--- src/NumberGuessingGameFuncComponent.js
+++ src/NumberGuessingGameFuncComponent.js
@@ -1,8 +1,28 @@
 import React from 'react'
 
 function NumberGuessingGameFuncComponent() {
+  const isCorrectGuess = latestGuess === numberToGuess;
+
+  const isGameOver =
+    isCorrectGuess || numberOfGuesses === MAX_ATTEMPTS;
   return (
-    <div>NumberGuessingGameFuncComponent Placeholder</div>
+    <div>
+      <h2>I'm thinking of a number from 1 to 100.</h2>
+      <h2>
+        Can you guess the number I am thinking of in {MAX_ATTEMPTS} tries?
+      </h2>
+      <GuessControl onGuess={handleGuess} />
+      {isGameOver && (
+        <GameOver hasWon={isCorrectGuess} onReset={handleReset} />
+      )}
+      {!isGameOver && (
+        <GuessMessage
+          guess={latestGuess}
+          numberToGuess={numberToGuess}
+          numberOfGuesses={numberOfGuesses}
+        />
+      )}
+    </div>
   )
 }
 
