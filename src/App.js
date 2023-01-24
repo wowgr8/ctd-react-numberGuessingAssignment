80edfc12a8d37bee086c395442db825f2d3f48ee
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-24 12:59:36 -0800

    add NumberGuessingGameFuncComponent and replace NumberGuessingGame class component version.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import NumberGuessingGame from "./NumberGuessingGame";
+import NumberGuessingGameFuncComponent from "./NumberGuessingGameFuncComponent";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NumberGuessingGame />
+        <NumberGuessingGameFuncComponent />
       </div>
     );
   }
