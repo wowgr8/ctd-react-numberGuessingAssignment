154e4dd2d9c4f1a0746b0631c71101aa89004a79
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2023-01-24 11:47:43 -0800

    add multi-line return from GuessControl.js - remove references to 'this'. Add useState for currentGuess. Add handleInputChange() and onSubmitGuess() - to be refactored WIP

--- src/GuessControlFuncComponent.js
+++ src/GuessControlFuncComponent.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function GuessControlFuncComponent() {
+  const [currentGuess, setCurrentGuess] = useState("")
+
+  handleInputChange(event) {
+    setCurrentGuess(event.target.value);
+  }
+
+  onSubmitGuess() {
+    // Since the values from an HTML input are strings by default,
+    //  convert to a number for the returned guess value
+    //  by passing in the string to the Number function.
+    // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+    this.props.onGuess(Number(this.state.currentGuess));
+    this.setState({ currentGuess: "" });
+  }
+
   return (
-    <div>GuessControlFuncComponent Placeholder</div>
+    <div>
+      <input
+        type="number"
+        value={currentGuess}
+        onChange={handleInputChange}
+      />
+      <Button onClick={onSubmitGuess}>Submit Guess</Button>
+    </div>
   )
 }
 
